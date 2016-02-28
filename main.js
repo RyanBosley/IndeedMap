4a1328a5cc18a702c6f507d04f802abbc4e12514
RyanBosley <ryanmbosley@gmail.com>
2016-02-28 13:18:38 -0800

    Removed jQuery. Refactoring.

--- main.js
+++ main.js
@@ -1,185 +1,130 @@
-var paging=document.getElementById('paging');
-var resultsSection=document.getElementById('results-section');
-var theForm=$('#theForm');
-var data,map,markers;
-
-function initMap(){
-    map = L.map('map',{center:new L.latLng(38.41055825094609,-93.33984375),zoom:4,home:true}).setView([38.37611542403604, -93.3837890625], 4);
-    L.tileLayer('http://api.tiles.mapbox.com/v4/examples.map-zr0njcqy/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoicnlhbm1ib3NsZXkiLCJhIjoiMkJqazZLbyJ9.nPS-SAuaRamw9TdSxsm3BA', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        maxZoom: 18
-    }).addTo(map);
-    map.options.center=map.getCenter();
+(function(L,JSONP){
+    var paging=document.getElementById('paging');
+    var resultsSection=document.getElementById('results-section');
+    var theForm=document.getElementById('theForm');
+    var btnNext=document.getElementById('btnNext');
+    var btnPrevious=document.getElementById('btnPrevious');
+    var data,map,markers;
     
-    markers = new L.MarkerClusterGroup();
-    map.addLayer(markers);
-}
-
-var CustomMarker = L.Marker.extend({ 
-    bindPopup: function(htmlContent, options) {                                    
-        if (options && options.showOnMouseOver) {                            
-            // call the super method
-            L.Marker.prototype.bindPopup.apply(this, [htmlContent, options]);
-            
-            // unbind the click event
-            this.off("click", this.openPopup, this);
-            
-            // bind to mouse over
-            this.on("mouseover", function(e) {                        
-                // get the element that the mouse hovered onto
-                var target = e.originalEvent.fromElement || e.originalEvent.relatedTarget;
-                var parent = this._getParent(target, "leaflet-popup");
-                // check to see if the element is a popup, and if it is this marker's popup
-                if (parent == this._popup._container){
-                    return true;
-                }                        
-                // show the popup
-                this.openPopup();                        
-            }, this);
-                    
-            // and mouse out
-            this.on("mouseout", function(e) {                        
-                // get the element that the mouse hovered onto
-                var target = e.originalEvent.toElement || e.originalEvent.relatedTarget;                        
-                // check to see if the element is a popup
-                if (this._getParent(target, "leaflet-popup")) {
-                    L.DomEvent.on(this._popup._container, "mouseout", this._popupMouseOut, this);
-                    return true;
-                }                        
-                // hide the popup
-                this.closePopup();                        
-            }, this);                        
-        }                    
-    },
- 
-    _popupMouseOut: function(e) {            
-        // detach the event
-        L.DomEvent.off(this._popup, "mouseout", this._popupMouseOut, this);
-        // get the element that the mouse hovered onto
-        var target = e.toElement || e.relatedTarget;            
-        // check to see if the element is a popup
-        if (this._getParent(target, "leaflet-popup")){
-            return true;
-        }            
-        // check to see if the marker was hovered back onto
-        if (target == this._icon){
-            return true;
-        }            
-        // hide the popup
-        this.closePopup();                
-    },
+    initMap();
+    
+    theForm.addEventListener('submit',onSubmit);
+    theForm.addEventListener('reset',onReset);
+    btnPrevious.addEventListener('click',previousResults);    
+    btnNext.addEventListener('click',nextResults);
+    
+    function initMap(){
+        map = L.map('map',{center:new L.latLng(38.41055825094609,-93.33984375),zoom:4,home:true}).setView([38.37611542403604, -93.3837890625], 4);
+        L.tileLayer('http://api.tiles.mapbox.com/v4/examples.map-zr0njcqy/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoicnlhbm1ib3NsZXkiLCJhIjoiMkJqazZLbyJ9.nPS-SAuaRamw9TdSxsm3BA', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 18
+        }).addTo(map);
+        map.options.center=map.getCenter();
         
-    _getParent: function(element, className) {                
-        var parent = element.parentNode;            
-        while (parent != null) {                    
-            if (parent.className && L.DomUtil.hasClass(parent, className)){
-                return parent;
-            }                
-            parent = parent.parentNode;                    
-        }            
-        return false;                
-    }     
-});
-
-function doSearch(what,where,start) {
-    var dfd=new $.Deferred(); 
-    $.ajax({
-        url: 'http://api.indeed.com/ads/apisearch',
-        jsonp: "callback",
-        dataType: "jsonp",
-        data: {
+        markers = new L.MarkerClusterGroup();
+        map.addLayer(markers);
+    }
+    
+    function doSearch(options,callback) {
+        JSONP.get('http://api.indeed.com/ads/apisearch',{
             publisher:'7389100658200406',
-            q:what,
-            l:where,
-            start:start || '',
+            q:options.what,
+            l:options.where,
+            start:options.start || '',
             v:'2',
             format:'json',
             latlong:1
-        },
-        success: function( response ) {
-            dfd.resolve(response);
-        }
-    });
-    return dfd;
-}
-
-function onSubmit(e) {
-    e.preventDefault();
+        }, callback);
+    }
     
-    var what=e.target[0].value;
-    var where=e.target[1].value;
-    doSearch(what,where).done(function(response){
-        data=response;
-        displayResults()
-    });
-}
-
-function displayResults() {
-    clearResults();
+    function onSubmit(e) {
+        e.preventDefault();
+        
+        doSearch({
+            what:e.target[0].value,
+            where:e.target[1].value
+        },function(response){
+            data=response;
+            displayResults()
+        });
+    }
     
-    data.results.forEach(function(result){  
-        if (result.latitude && result.longitude) {
-            var marker = new CustomMarker([result.latitude,result.longitude]);
-            var content='<a href="' + result.url + '" target="_blank">' + result.jobtitle +
-                '</a><br/><span>' + result.company + '</span><br/><span>' + result.formattedLocationFull + '<span>';
-            marker.bindPopup(content,{showOnMouseOver: true});
-            markers.addLayer(marker);
-        }
-    });
+    function displayResults() {
+        clearResults();
+        
+        data.results.forEach(function(result){  
+            if (result.latitude && result.longitude) {
+                var marker = new CustomMarker([result.latitude,result.longitude]);
+                var content='<a href="' + result.url + '" target="_blank">' + result.jobtitle +
+                    '</a><br/><span>' + result.company + '</span><br/><span>' + result.formattedLocationFull + '<span>';
+                marker.bindPopup(content,{showOnMouseOver: true});
+                markers.addLayer(marker);
+            }
+        });
+        
+        if (data.results.length) {
+            map.fitBounds(markers.getBounds());
+        
+            resultsSection.style.display='block';
+            paging.innerHTML='Jobs ' + data.start + ' to ' + data.end + ' of ' + data.totalResults;
+            btnNext.style.display=data.end < data.totalResults ? 'inline-block' : 'none';
+            btnPrevious.style.display=data.pageNumber > 0 ? 'inline-block' : 'none';
+        }        
+    }
     
-    if (data.results.length) {
-        map.fitBounds(markers.getBounds());
+    function getParent(element, className) {		
+        var parent = element.parentNode;        
+        while (parent != null) {                
+            if (parent.className && L.DomUtil.hasClass(parent, className))
+                return parent;            
+            parent = parent.parentNode;                
+        }        
+        return false;            
+    }
     
+    function clearResults() {
+        markers.clearLayers();
+        paging.innerHTML='';
         resultsSection.style.display='block';
-        paging.innerHTML='Jobs ' + data.start + ' to ' + data.end + ' of ' + data.totalResults;
-        data.end < data.totalResults ? $('#btnNext').show() : $('#btnNext').hide();
-        data.pageNumber > 0 ? $('#btnPrevious').show() : $('#btnPrevious').hide();
-    }        
-}
-
-function getParent(element, className) {		
-    var parent = element.parentNode;        
-    while (parent != null) {                
-        if (parent.className && L.DomUtil.hasClass(parent, className))
-            return parent;            
-        parent = parent.parentNode;                
-    }        
-    return false;            
-}
-
-function clearResults() {
-    markers.clearLayers();
-    paging.innerHTML='';
-    resultsSection.style.display='block';
-}
-
-function onReset(e){
-    e.preventDefault();
-    $('form input').attr('value','');
-    clearResults();
-    resultsSection.style.display='none';
-}
-
-initMap();
-
-theForm.on('submit',onSubmit);
-theForm.on('reset',onReset);
-
-$('#btnPrevious').on('click',function(){
-    if (data.pageNumber > 0) {
-        var start=data.pageNumber===1 ? '' : (data.pageNumber - 1) * 10;
-        doSearch(data.query,data.location,start).done(function(response){
-            data=response;
-            displayResults();
-        });
     }
-});
-
-$('#btnNext').on('click',function(){
-    if (data.end < data.totalResults) {
-        doSearch(data.query,data.location,data.end).done(function(response){
-            data=response;
-            displayResults();
-        });
+    
+    function onReset(e){
+        e.preventDefault();
+        var formInputs=document.querySelectorAll('form input');
+        for(var i=0,formInput;formInput=formInputs[i];i++){
+            formInput.value='';
+        }
+        clearResults();
+        resultsSection.style.display='none';
     }
-});
+    
+    function previousResults() {
+        if (data.pageNumber > 0) {
+            var start=data.pageNumber===1 ? '' : (data.pageNumber - 1) * 10;
+            var options={
+                what: data.query,
+                where: data.location,
+                start: start
+            };
+            doSearch(options,function(response){
+                data=response;
+                displayResults();
+            });
+        }
+    }
+    
+    function nextResults() {
+        if (data.end < data.totalResults) {
+            var options={
+                what: data.query,
+                where: data.location,
+                start: data.end
+            };
+            doSearch(options,function(response){
+                data=response;
+                displayResults();
+            });
+        }
+    }
+}(L,JSONP));
+
